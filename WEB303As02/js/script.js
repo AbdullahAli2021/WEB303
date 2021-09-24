442070a49f48d6f0dc3bcc943be65d4900625c66
Abdulah Ali <w0782917@myscc.ca>
2021-09-24 19:47:24 -0400

    assignment 2
    assignment 2

---
+++ WEB303As02/js/script.js
@@ -0,0 +1,10 @@
+// WEB303 Assignment 2
+// Abdullah Ali    
+
+$(function () {
+        $("#vprospect").click(function(){
+        $("#solution").load("prospect.html").slideDown(500);
+    });
+});
+
+
