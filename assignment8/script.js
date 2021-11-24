eb9619ff991d3d63fd0f1a7260c8a3fa25c0497f
Abdulah Ali <w0782917@myscc.ca>
2021-11-24 18:53:50 -0500

    assignment 8

--- assignment8/script.js
+++ assignment8/script.js
@@ -4,16 +4,35 @@ $(function() {
             console.log(value.name);
             $('table#characters').append(
                 `<tr>
-                <td>${value.name}</td>
+                <td class= "name">${value.name}</td>
                 <td>${value.power}</td>
                 <td>${value.country}</td>
                 <td>${value.age}</td>
                 </tr>`);
         });
 
-        
+
     
     });
+
+    ('.name').addClass('active');
+
+    $("#search").keyup(function () {
+        var data = this.value.toUpperCase().split(" ");    
+        $("#characters").find('.name').each(function (index, elem) {
+        var $elem = $(elem);
+            for (var d = 0; d < data.length; ++d) {
+                    //alert($elem.val());
+                // Highlight
+                if ($elem.val().toUpperCase().indexOf(data[d].toUpperCase()) != -1) {
+                    $elem.addClass('active');
+    
+                } else {
+                    $elem.removeClass('active');   
+                }
+            }
+        })     
+    }) 
 
 }
 )
