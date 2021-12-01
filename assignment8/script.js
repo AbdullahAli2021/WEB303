d2c743e6012497ea60a51814b93a45da466e4071
Abdulah Ali <w0782917@myscc.ca>
2021-12-01 00:50:30 -0500

    assignment 8

--- assignment8/script.js
+++ assignment8/script.js
@@ -2,36 +2,75 @@ $(function() {
     $.getJSON("characters.json").done(function (data) {
         $.each(data.characters, function (index, value) {
             console.log(value.name);
-            $('table#characters').append(
+            $('.bb').append(
                 `<tr>
                 <td class= "name">${value.name}</td>
                 <td>${value.power}</td>
                 <td>${value.country}</td>
-                <td>${value.age}</td>
+                <td>${value.DateofBirth}</td>
                 </tr>`);
         });
 
+        $("#search").keyup(function () {
+            var data = this.value.toLowerCase();    
+            $('.name').each(function () {
+            var $name = $(this);
+            var tname = $name.text();
+                    if (tname.toLowerCase().indexOf(data) != -1) {
+                        $name.addClass('active');
+        
+                    } else {
+                        $name.removeClass('active');   
+                    }
+                
+            })     
+        }) 
 
+        $(".first").click(function () {
+            var count = 0;
+            $('.name').each(function () {
+                var $name = $(this);
+                var tname = $name.text();
+                
+                if ("a"<=tname.charAt(0).toLowerCase() && tname.charAt(0).toLowerCase()<="m") {
+                    //console.log(tname.charAt(0).toLowerCase());
+                    $name.parent().show();
+                    count++;
+                } else {
+                    //console.log(tname.charAt(0).toLowerCase());
+                    $name.parent().hide();
+                }
+
+
+            });
+            $(".first").append(` (${count})`)
+        });
+
+        $(".second").click(function () {
+            var count = 0;
+            $('.name').each(function () {
+                var $name = $(this);
+                var tname = $name.text();
+                
+                if ("n"<=tname.charAt(0).toLowerCase() && tname.charAt(0).toLowerCase()<="z") {
+                    //console.log(tname.charAt(0).toLowerCase());
+                    $name.parent().show();
+                    count++;
+                } else {
+                    //console.log(tname.charAt(0).toLowerCase());
+                    $name.parent().hide();
+                }
+
+
+            });
+            $(".second").append(` (${count})`)
+        });
+
+    
     
     });
 
     
 
-    $("#search").keyup(function () {
-        var data = this.value.toLowerCase();    
-        $('tr').each(function (index, elem) {
-        var $elem = $(elem);
-            for (var d = 0; d < data.length; ++d) {
-                    //alert($elem.val());
-                // Highlight
-                if ($elem.val().toUpperCase().indexOf(data[d].toUpperCase()) != -1) {
-                    $elem.addClass('active');
-    
-                } else {
-                    $elem.removeClass('active');   
-                }
-            }
-        })     
-    }) 
 
 })
